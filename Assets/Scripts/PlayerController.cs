0025d4fcc01f543f2ee8c22ddcf44f4ba1ac8191
F-Style <103450297+just-f-style@users.noreply.github.com>
2024-02-11 00:13:48 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float gravity;
+    public float jumpForce;
+    public float speed;
+    public float mass;
+
+    private Vector3 _moveVector;
+    private float _fallVelocity;
+    private CharacterController _characterController;
+    public static bool _isMove;
+
+	void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _fallVelocity += gravity * Time.fixedDeltaTime;
+
+        _characterController.Move(_moveVector * speed * Time.fixedDeltaTime);
+
+        _characterController.Move(Vector3.down * _fallVelocity * mass * Time.fixedDeltaTime);
+    }
+	private void Update()
+	{
+        _moveVector = Vector3.zero;
+
+        if (!Input.GetKey(KeyCode.LeftAlt))
+        {
+			if (_characterController.isGrounded)
+			{
+				if (_fallVelocity > 10)
+				{
+					_fallVelocity = 0;
+				}
+				speed = 10f;
+
+				if (Input.GetKeyDown(KeyCode.Space))
+				{
+					_fallVelocity = -jumpForce;
+				}
+
+				if (Input.GetKey(KeyCode.W)) _moveVector += transform.forward;
+				if (Input.GetKey(KeyCode.S)) _moveVector -= transform.forward;
+				if (Input.GetKey(KeyCode.D)) _moveVector += transform.right;
+				if (Input.GetKey(KeyCode.A)) _moveVector -= transform.right;
+			}
+			else speed = 2.5f;
+		}
+		if (_moveVector != Vector3.zero) _isMove = true;
+		else _isMove = false;
+	}
+}
---
+++ Assets/Scripts/СамеraRotation.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class СамеraRotation : MonoBehaviour
+{
+	public float RotationSpeed;
+
+	public float MinRotationAngle;
+	public float MaxRotationAngle;
+	public int RotateBodyAngle;
+	public float bodyRotationSpeed;
+
+	public Transform CameraAxisTransform;
+	public Transform BodyOriginAxisTransform;
+	public Transform Body;
+
+	private void CameraRotateXAngle()
+	{
+		var newXRotationAngle = CameraAxisTransform.localEulerAngles.x - Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse Y");
+		if (newXRotationAngle > 180) newXRotationAngle -= 360;
+		newXRotationAngle = Mathf.Clamp(newXRotationAngle, MinRotationAngle, MaxRotationAngle);
+		CameraAxisTransform.localEulerAngles = new Vector3(newXRotationAngle, CameraAxisTransform.localEulerAngles.y, CameraAxisTransform.localEulerAngles.z);
+		if (PlayerController._isMove)
+		{
+			BodyOriginAxisTransform.localEulerAngles = new Vector3(BodyOriginAxisTransform.localEulerAngles.x, CameraAxisTransform.localEulerAngles.y, BodyOriginAxisTransform.localEulerAngles.z);
+			if (Input.GetKey(KeyCode.W))
+				RotateBodyAngle = 0;
+			if (Input.GetKey(KeyCode.D))
+				RotateBodyAngle = 90;
+			if (Input.GetKey(KeyCode.S))
+				RotateBodyAngle = 180;
+			if (Input.GetKey(KeyCode.A))
+				RotateBodyAngle = 270;
+
+			if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W))
+				RotateBodyAngle = 45;
+			if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S))
+				RotateBodyAngle = 135;
+			if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+				RotateBodyAngle = 225;
+			if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+				RotateBodyAngle = 315;
+
+			if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
+				RotateBodyAngle = 180;
+
+			Body.localEulerAngles = new Vector3(BodyOriginAxisTransform.localEulerAngles.x, Mathf.LerpAngle(Body.localEulerAngles.y, RotateBodyAngle, bodyRotationSpeed * Time.deltaTime), BodyOriginAxisTransform.localEulerAngles.z);
+
+		}
+	}
+	private void CameraRotateYAngle()
+	{
+		var VectorYRotation = transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X");
+		transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, VectorYRotation, transform.localEulerAngles.z);
+
+		
+	}
+	private void CursorHideAndLock()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+	private void CursorShowAndUnlock()
+	{
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = true;
+	}
+
+	void Update()
+	{
+		if (!Input.GetKey(KeyCode.LeftAlt))
+		{
+			CameraRotateXAngle();
+			CameraRotateYAngle();
+			CursorHideAndLock();
+		}
+		else CursorShowAndUnlock();
+	}
+
+}
+
